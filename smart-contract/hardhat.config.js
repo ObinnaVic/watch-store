9b583cefc5567edaa49a4948bee1bc821f98eee8
Victor Nkire <nkirevictor77@gmail.com>
2023-08-20 23:22:07 +0100

    hardhat config update

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,7 +1,6 @@
 //https://eth-goerli.g.alchemy.com/v2/HpfbCu8XwuaeSPA5_a4V6Kn2ILuESa3l
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
-console.log(process.env.PRIVATE_KEY);
 
 module.exports = {
   solidity: "0.8.9",
